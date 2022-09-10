71c3a1e1e8fa5cd9d7afa96f12b4a53718939c24
Rodas <rodasbest@gmail.com>
2022-09-09 17:17:17 -0700

    number combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,10 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(j + '0');
-			putchar(' ');
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
