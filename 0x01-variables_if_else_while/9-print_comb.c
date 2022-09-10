e2ba0a9227fba359334926a7cc86c6f9661cfb43
Rodas <rodasbest@gmail.com>
2022-09-09 17:10:26 -0700

    number combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i);
+			putchar(',');
+			putchar(j);
+			putchar(' ');
+		}
+	}
+	return (0);
+}
