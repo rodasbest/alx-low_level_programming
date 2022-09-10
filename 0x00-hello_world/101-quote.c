bc37a4f666b712758c76f2e02606c6a8fafade84
Rodas <rodasbest@gmail.com>
2022-09-09 20:47:34 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,14 @@
 /***
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Description: writes the message to the standard error
+ *
+ * Return: Always 0 (Success)
  */
-/* main function is where everything starts */
+
 int main(void)
 {
+	/* Some description about the function */
 	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			39, 1, stderr);
 	return (1);
