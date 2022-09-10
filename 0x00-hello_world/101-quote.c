70e53cc2420badbb7652fb157dd54af4b2baf1b3
Rodas <rodasbest@gmail.com>
2022-09-09 20:32:30 -0700

    unix is special

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/***
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+/* main function is where everything starts */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			39, 1, stderr);
+	return (1);
+}
