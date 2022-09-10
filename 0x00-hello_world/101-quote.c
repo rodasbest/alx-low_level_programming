54100fa814179e0babb0f88150d8de6048339263
Rodas <rodasbest@gmail.com>
2022-09-10 02:10:15 -0700

    standrad error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 
-/***
+/**
  * main - Entry point
  *
  * Description: writes the message to the standard error
  *
- * Return: Always 0 (Success)
+ * Return: 1 
  */
 
 int main(void)
 {
-	/* Some description about the function */
 	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			39, 1, stderr);
 	return (1);
