5c402d7fded6c0ca5ec7869d90e615beebc1f324
Rodas <rodasbest@gmail.com>
2022-09-13 13:47:33 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@
 
 void times_table(void)
 {
-	int x, y, z, u, d;
+	int x, y, z, i, j;
 
 	for (x = 0; x <= 9; x++)
 	{
@@ -16,21 +16,21 @@ void times_table(void)
 			z = x * y;
 			if (z > 9)
 			{
-				u = z % 10;
-				d = (z - u) / 10;
+				i = z % 10;
+				j = (z - i) / 10;
 
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(j + '0');
+				_putchar(i + '0');
 			}
 			else
 			{
 				if (y != 0)
 				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
 			
 				_putchar(z + '0');
