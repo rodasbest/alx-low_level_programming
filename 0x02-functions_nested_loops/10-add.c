0a0099cc2118ed8705c538c73c3384deff68cd9a
Rodas <rodasbest@gmail.com>
2022-09-13 12:10:01 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @num1: first integer
+ *
+ * @num2: second integer
+ *
+ * Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
