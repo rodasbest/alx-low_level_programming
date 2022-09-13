d4c3239b3dffcf79a2261b77b89a2db54d4f3a05
Rodas <rodasbest@gmail.com>
2022-09-13 08:47:57 -0700

    number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - sign of a number
+ *
+ * @n: integer
+ *
+ * Return: 1 positive 0 zero -1 negative
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+}
