2e5179eb839716519e29cbd6c64f3d24f266de31
Rodas <rodasbest@gmail.com>
2022-09-13 07:55:08 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,12 @@ void print_alphabet_x10(void)
 	int i;
 	char ch;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
-			if (ch == 'z')
-			{
-				_putchar('\n');
-			}
 		}
+		_putchar('\n');
 	}
 }
