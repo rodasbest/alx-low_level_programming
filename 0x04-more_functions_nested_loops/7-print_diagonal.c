f1930d9411ae7fa99d8e8050daf165b1c21fdc44
Rodas <rodasbest@gmail.com>
2022-09-16 03:03:50 +0000

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+nclude "main.h"
+
+/**
+ * print_diagonal - print a diagonal of lengeth n..
+ * @n: lenget of line
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			for (i = 0; i < j; i++)
+			{
+				_putchar(' ');
+			}
+																		_putchar('\\');
+																		_putchar('\n');
+																	}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
