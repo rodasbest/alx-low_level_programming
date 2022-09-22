3f2df30b38f270246a12ad0a5bfe00e1302dc8d8
Rodas <rodasbest@gmail.com>
2022-09-22 17:18:16 +0000

    task 5 done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * string_toupper - changes all lowercase letters of a string to uppercase
+  *
+  * @str: input string
+  *
+  *Return: All uppercase string
+  */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+
+	return (str);
+}
