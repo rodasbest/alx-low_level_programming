bad9d103857d548992772cea350a2c0c5a17f8fa
Rodas <rodasbest@gmail.com>
2022-09-22 12:54:05 +0000

    task 2 done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,9 +13,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (i < n && *(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	while (i < n)
+	{
+		*(dest + i) = '\0';
 		i++;
 	}
 
