2157b8aed155271d72ce7b11871d0f74db7730dd
Rodas <rodasbest@gmail.com>
2022-09-25 12:15:10 +0000

    task 13 done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * sum_of_multiples - calcutlate sum of multiples of 3 and 5
+ *
+ * @n: natural number range
+ *
+ * Return: nothing
+ */
+
+void sum_of_multiples(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +12,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+void sum_of_multiples(void);
+
