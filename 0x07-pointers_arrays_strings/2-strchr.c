e1f531a31f3cbd784467f3cd09676dd9c9e84919
Rodas <rodasbest@gmail.com>
2022-09-26 18:04:47 +0000

    tried running with iterator

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,16 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-		if (*s == c)
+		if (*(s + i) == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		s++;
+		i++;
 	}
 
 	return ('\0');
