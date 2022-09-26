327a4759944919285af32780587255c19807145b
Rodas <rodasbest@gmail.com>
2022-09-26 17:01:16 +0000

    memory copy area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ *
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: unsigned integer of bytes to be copied
+ *
+ * Return: dest pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	return (dest);
+}
