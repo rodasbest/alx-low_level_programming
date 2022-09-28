eaa48e0375566cf55769ac80f33d25851319c83f
Rodas <rodasbest@gmail.com>
2022-09-28 21:07:52 +0000

    task 5 done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * sqrt_helper - helper function to allow use of another variable
+ * @n: number to be evaluated
+ * @m: potential square root of m.
+ *
+ * Return: natural square root of n, or -1 if no square root exists
+ */
+int sqrt_helper(int n, int m)
+{
+	if (n < 0)
+		return (-1);
+
+	if(m * m == n)
+	{
+		return (m);
+	}
+	else if (m != n)
+	{
+		return (sqrt_helper(n, m + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number
+ *
+ * @n: the number to calculate
+ *
+ * Return: square root, or
+ * -1 if it does not have natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_helper(n, 0));
+}
