bb013e54fd3a5b082d78fecfaf9904cec664a5b4
Rodas <rodasbest@gmail.com>
2022-09-30 20:53:30 +0000

    task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * @argc: argument count
+ * @argv: array of argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+0-whatsmyname > prints its name, followed by a new line.
+1-args > prints the number of arguments passed into it.
+2-args > prints all arguments it receives.
+3-mul > multiplies two numbers.
+4-add > adds positive numbers.
