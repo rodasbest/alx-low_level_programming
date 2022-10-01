266cdadc085f1eb0f77f486ae8d7a1ef936778ac
Rodas <rodasbest@gmail.com>
2022-10-01 19:27:57 +0000

    removed the space after 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,29 +11,30 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i != 100)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			if (i != 100)
 			{
-				printf("FizzBuzz");
-				printf(" ");
-			}
-			else if (i % 3 == 0)
-			{
-				printf("Fizz");
-				printf(" ");
-			}
-			else if (i % 5 == 0)
-			{
-				printf("Buzz");
-				printf(" ");
-			}
-			else
-			{
-				printf("%d ", i);
 				printf(" ");
 			}
 		}
+		else
+		{
+			printf("%d ", i);
+			printf(" ");
+		}
+		
 	}
 	printf("\n");
 
