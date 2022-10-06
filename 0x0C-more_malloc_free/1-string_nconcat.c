ca2eae68699cb6a25afe49e4db32efd374ca78ac
Rodas <rodasbest@gmail.com>
2022-10-06 18:51:53 +0000

    changed malloc size

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	concat = malloc(sizeof(char) * (strleng(s1) + n + 1));
+	concat = malloc(sizeof(char) * (strleng(s1) + 1));
 
 	if (concat == NULL)
 		return (NULL);
